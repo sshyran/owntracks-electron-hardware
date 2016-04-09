43d976737a5a973d29942c127e3370e4dc2c251a
Jan-Piet Mens <jpmens@gmail.com>
2016-04-09 14:09:51 +0200

    rewrite for Particle publish()

--- backend/obtain.py
+++ backend/obtain.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import paho.mqtt.client as paho   # pip install paho-mqtt
-import requests
+import paho.mqtt.publish as mqtt
+from sseclient import SSEClient
 import json
-import ssl
 import ConfigParser
+import ssl
 import time
-import logging
-import socket
 import sys
 import os
 
@@ -16,72 +14,7 @@ __author__    = 'Jan-Piet Mens <jpmens()gmail.com>'
 __copyright__ = 'Copyright 2016 Jan-Piet Mens'
 __license__   = """GPL2"""
 
-def get_cloud(mqttc, token, device_id):
-
-    payload = None
-    params = {
-        'access_token' : token,
-    }
-
-    r = requests.get('https://api.particle.io/v1/devices/%s/status' % device_id,
-        params=params)
-
-    if r.status_code != 200:
-        print r.status_code
-        print r.text
-        mqttc.publish(topic + '/warn', "status=%s" % r.status_code, qos=2, retain=False)
-        return None
-
-    print r.text
-
-    try:
-        data = json.loads(r.content);
-    except:
-        mqttc.publish(topic + '/warn', "cannot parse JSON" % r.status_code, qos=2, retain=False)
-        print "Cannot parse JSON"
-        return None
-
-    if 'result' in data:
-        try:
-            result = data['result']
-            tst,lat,lon,vcell,soc,uptime = result.split(',')
-            if int(tst) < 1:
-                mqttc.publish(topic + '/warn', str("ignore-zero %s" % result), qos=2, retain=False)
-                print "Ignoring zeroed results:", result
-                return None
-            payload = {
-                '_type':        'location',
-                'tst':          int(tst),
-                'lat':          float(lat),
-                'lon':          float(lon),
-                'tid':          device_id[-2:],
-                'batt':         float(vcell),       # Battery voltage
-                'soc':          float(soc),         # State of Charge (in %)
-                'up':           int(uptime),
-            }
-
-            return json.dumps(payload)
-
-        except:
-            raise
-
-    return payload
-
-def on_disconnect(mosq, userdata, rc):
-
-    reasons = {
-       '0' : 'Connection Accepted',
-       '1' : 'Connection Refused: unacceptable protocol version',
-       '2' : 'Connection Refused: identifier rejected',
-       '3' : 'Connection Refused: server unavailable',
-       '4' : 'Connection Refused: bad user name or password',
-       '5' : 'Connection Refused: not authorized',
-    }
-    reason = reasons.get(rc, "code=%s" % rc)
-    print "Disconnected: ", reason
-
-def on_log(mosq, userdata, level, string):
-    print(string)
+eventname = 'owntracks'
 
 cf = ConfigParser.RawConfigParser()
 cf.read("electron.ini")
@@ -96,41 +29,73 @@ topic       = cf.get('mqtt', 'topic')
 token       = cf.get('particle', 'token')
 device_id   = cf.get('particle', 'device_id')
 
+auth = {
+    "username" : username,
+    "password" : password
+}
+tls = {
+    "ca_certs" : tls_cert,
+    "tls_version" : ssl.PROTOCOL_TLSv1,
+}
+
+will = {
+    "topic":    topic,
+    "qos":      2,
+    "retain":   False,
+    "payload":  json.dumps({'_type':"lwt", "tst":int(time.time())}),
+}
+
 clientid = 'owntracks-electron-pub-%s' % os.getpid()
-protocol=paho.MQTTv31  # 3
-protocol=paho.MQTTv311 # 4
+protocol=3
 
-mqttc = paho.Client(clientid, clean_session=True, userdata=None, protocol=protocol)
-mqttc.on_disconnect = on_disconnect
-mqttc.on_log = on_log
-
-mqttc.tls_set(tls_cert)
-mqttc.username_pw_set(username, password)
-
-# Set OwnTracks-compatible LWT
-mqttc.will_set(topic,
-    payload=json.dumps({'_type':"lwt", "tst":int(time.time())}),
-    qos=0, retain=False)
-
-mqttc.loop_start()
-
-mqttc.connect(hostname, port, 60)
-
-while True:
+def process(csv):
     try:
-        payload = get_cloud(mqttc, token, device_id)
-        if payload is not None:
-            (res, mid) =  mqttc.publish(topic, payload, qos=2, retain=True)
-    except socket.error:
-        print("MQTT server disconnected; sleeping")
-        time.sleep(5)
-    except KeyboardInterrupt:
-        mqttc.disconnect()
-        mqttc.loop_stop()
-        sys.exit(0)
-    except:
+        tst,lat,lon,soc,interval = csv.split(',')
+        if int(tst) < 1:
+            print "Ignoring zeroed results:", result
+        payload = {
+            '_type':        'location',
+            'tst':          int(tst),
+            'lat':          float(lat),
+            'lon':          float(lon),
+            'tid':          device_id[-2:],
+            'batt':         float(soc),       # State of Charge in %
+            '_interval':           int(interval),
+        }
+
+        mqtt.single(topic, json.dumps(payload), qos=2, retain=True,
+            hostname=hostname, port=int(port), client_id=clientid,
+            auth=auth, will=will,
+            tls=tls, protocol=int(protocol))
+
+    except Exception, e:
+        print str(e)
         raise
 
-    time.sleep(10)
 
-mqttc.loop_stop()
+messages = SSEClient('https://api.spark.io/v1/events/%s?access_token=%s' % (eventname, token))
+
+for msg in messages:
+    event = str(msg.event).encode('utf-8')
+    data = str(msg.data).encode('utf-8')
+    # print "Event=", event
+    # print "Data=[",data,"]"
+
+    if event == eventname:
+        print "Data=",data
+        try:
+            #   {
+            #     "coreid": "nnnnnnnnnnnnnnnnnnnnnnnn",
+            #     "published_at": "2016-04-09T08:29:40.532Z",
+            #     "ttl": "600",
+            #     "data": "1460190580,xx.543674,yy.422098,3.7,30.8,31"
+            #   }
+
+            j = json.loads(data)
+
+            if 'coreid' in j and 'data' in j:
+                if j['coreid'] == device_id:
+                    process(j['data'])
+        except:
+            continue
+
